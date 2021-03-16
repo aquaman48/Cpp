82a50662672f99526244df2a6ab52f009781206d
aquaman48 <63140071+aquaman48@users.noreply.github.com>
2021-03-16 16:22:32 -0700

    Simple Math in C++

---
+++ Basics/MathExercise.cpp
@@ -0,0 +1,16 @@
+//Week One Exercise Part 2 Question 5
+#include<iostream>
+using namespace std;
+int main()
+{
+	double num, num2;
+	cout << "Enter first value: ";
+	cin >> num;
+	cout << "Enter second value: ";
+	cin >> num2;
+	cout << "The sum of " << num << " and " << num2 << " is " << (num + num2) << endl;
+	cout << "The difference of " << num << " and " << num2 << " is " << (num - num2) << endl;
+	cout << "The product of " << num << " and " << num2 << " is " << (num * num2) << endl;
+	cout << "The quotient of " << num << " and " << num2 << " is " << (num / num2) << endl;
+	return 0;
+}
