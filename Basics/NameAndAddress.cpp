399d6a819f733e93cde8292c2203cea5cdebf969
aquaman48 <63140071+aquaman48@users.noreply.github.com>
2021-03-18 17:56:36 -0700

    Privacy Update

--- Basics/NameAndAddress.cpp
+++ Basics/NameAndAddress.cpp
@@ -1,11 +1,11 @@
-//Question 6
+//Name and address in C++
 #include<iostream>
 using namespace std;
 int main()
 {
 	cout << "Nicholas Waterman" << endl;
-	cout << "Market St" << endl;
-	cout << "San Diego, CA" << endl;
+	cout << "123 EveryLane Rd." << endl;
+	cout << "Sandy Beach, CA" << endl;
 	return 0;
 
 }
