b5d934e789d5761c12002263a72a9450cfaee2dd
aquaman48 <63140071+aquaman48@users.noreply.github.com>
2021-03-18 17:55:11 -0700

    Create NameAndAddress.cpp

---
+++ Basics/NameAndAddress.cpp
@@ -0,0 +1,11 @@
+//Question 6
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout << "Nicholas Waterman" << endl;
+	cout << "Market St" << endl;
+	cout << "San Diego, CA" << endl;
+	return 0;
+
+}
