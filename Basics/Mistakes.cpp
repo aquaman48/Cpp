558de9c34dc6f0fd582d8cfc81d943c0296b79e9
aquaman48 <63140071+aquaman48@users.noreply.github.com>
2021-03-26 17:44:05 -0700

    If error
    IF an error comes from user input it will prompt them.

---
+++ Basics/Mistakes.cpp
@@ -0,0 +1,47 @@
+//Week 3 - PART 2: (B)-Using C++ Functions
+//Question 3
+//Nicholas Waterman
+#include<iostream>
+using namespace std;
+int main()
+{
+	const int END = 10;
+	int multiplicationTable();
+	int num;
+	int x;
+	int math;
+	//for (x = 2; x <= END; ++x)
+	//{
+		cout << "Let's do some maths!" << endl;
+		cout << "Please enter any valid whole number: ";
+		cin >> num;
+	
+	//}
+		math = num * 2;
+
+
+int multiplicationTable()
+
+	//int x;
+	//int num;
+	//const int START = 2;
+	//const int END = 10;
+	//int math;
+	//for (x = 2; x <= END; ++x)
+	//{
+	//	//cout << "Let's do some maths!" << endl;
+	//	cout << "Please enter any valid whole number: " << endl;
+	//	cin >> num;
+	//}
+	//math = num * x;
+		if (x = 2; x <= END; ++x)
+		{
+			cout << num << " times " << x << " is " << math << endl;
+		}
+	
+
+	
+
+	return num;
+   
+}
