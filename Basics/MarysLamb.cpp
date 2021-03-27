56fca4147a80c0f0b8f3c474298f74795ad7cf4f
aquaman48 <63140071+aquaman48@users.noreply.github.com>
2021-03-26 17:41:55 -0700

    function example

---
+++ Basics/MarysLamb.cpp
@@ -0,0 +1,18 @@
+//Week 3-PART 2: (B) - Using C++ Functions
+//Question 1
+//Nicholas Waterman
+#include<iostream>
+using namespace std;
+int main()
+{
+	void marysLamb();
+	marysLamb();
+	return 0;
+}
+void marysLamb()
+{
+	cout << "Mary had a little lamb" << endl;
+	cout << "It's fleece was white as snow" << endl;
+	cout << "And everywhere that Mary went" << endl;
+	cout << "The lamb was sure to go!" << endl;
+}
