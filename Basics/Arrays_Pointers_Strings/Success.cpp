f407508ad4302b1e16d9d285707df39d5c9eb2c1
aquaman48 <63140071+aquaman48@users.noreply.github.com>
2021-03-31 18:03:26 -0700

    Incorporating functions

---
+++ Basics/Arrays_Pointers_Strings/Success.cpp
@@ -0,0 +1,33 @@
+//Week 3 - PART 2: (B)-Using C++ Functions
+//Question 10
+//Nicholas Waterman
+#include<iostream>
+using namespace std;
+int main()
+{
+	const int LIMIT = 4;
+	int userInput[5];
+	int x, uinput, solution;
+	int quadruple();
+	uinput = quadruple();
+	for (x = 0; x <= LIMIT; ++x)
+	{
+		cout << " You have entered " << uinput << endl;
+		cout << uinput << " times four is " << uinput * 4 << endl;
+	}
+	return 0;
+}
+int quadruple()
+{
+	const int LIMIT = 4;
+	const int SIZE = 5;
+	int usersInput[SIZE]{ usersInput[0] };
+	int x, uinput;
+	uinput = usersInput[x];
+	for (x = 0; x <= LIMIT; ++x)
+	{
+		cout << "Please enter five whole numbers, one at a time " << (x + 1) << " >> ";
+		cin >> usersInput[x];
+	}
+	return uinput;
+}
